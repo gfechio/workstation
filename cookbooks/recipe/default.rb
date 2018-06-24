548c9dcc80ed72ddf8ed8b32664c52d5c8a80a91
gfechio <gfechio@gmail.com>
2018-06-24 17:28:33 +0200

    End to each

--- cookbooks/recipe/default.rb
+++ cookbooks/recipe/default.rb
@@ -10,10 +10,10 @@ end
 package %w(vim git tmux firefox terminator)
 
 %w[ /home/gfechio/Git /home/gfechio/GitHub /home/gfechio/Go ].each do |path|
-directory path do
-  owner 'gfechio'
-  recursive true
-  action :create
-  mode '0755'
+  directory path do
+    owner 'gfechio'
+    recursive true
+    action :create
+    mode '0755'
+  end
 end
-
