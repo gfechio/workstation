5a17907604eb51d1a87cdfb79036ed47c0c41a7b
gfechio <gfechio@gmail.com>
2018-06-26 11:24:57 +0200

    Configureing dotfiles

--- cookbooks/recipe/default.rb
+++ cookbooks/recipe/default.rb
@@ -21,3 +21,15 @@ end
 execute 'set_caps_lock_to_ctrl' do
   command 'setxkbmap -option caps:ctrl_modifier'
 end
+
+#Dotfiles Config (TMUX ALIAS VIM ZSH BASH)
+git '~/GitHub/gfechio'  do
+  repository 'https://github.com/gfechio/dotfiles.git'
+  revision 'master'
+  action :sync
+end
+
+execute 'move_dotfiles' do
+  command 'cp -r ~/GitHub/gfechio/dotfiles/.* ~'
+end
+
